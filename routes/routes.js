02eb592ba3b499ce188808f556fac46e95889345
joeco fong <joecofg@gmail.com>
2019-08-20 22:13:24 -0700

    requesting from balldontlie api

--- routes/routes.js
+++ routes/routes.js
@@ -3,18 +3,20 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/', function(req, res, next) {
-  request.get("https://www.balldontlie.io/api/v1/players", (error, response, data) => {
+  let page = 0; 
+  let per_page = 100;
+
+  let allPlayersRequest = `https://www.balldontlie.io/api/v1/players?page=${page}&per_page=${per_page}`
+
+  request.get(allPlayersRequest, (error, response, data) => {
     const parsedData = JSON.parse(data);
     res.send(parsedData);
 
+    let allPlayersPerPage = parsedData.data;
 
-    let allPlayers = parsedData.data;
-    let metaData = parsedData.meta;
-    
-    allPlayers.forEach((player) => {
+    allPlayersPerPage.forEach((player) => {
       console.log(player.team.city);
     })
-
   })
 });
 
